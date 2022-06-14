5bde98e807cf0aa7fcca890f0a4d33f21d216211
gilsoaresone <61061428+gilsoaresone@users.noreply.github.com>
2022-06-14 12:31:59 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const userRoute = require("./routes/user");
+const express =  require ('express')
 
-// settings
-const app = express();
-const port = process.env.PORT || 3000;
+const userRoutes = require('./routes/user')
 
-// middlewares
-app.use(express.json());
-app.use("/api", userRoute);
+ const app = express()
+const mongoose = require("mongoose")
+require('dotenv').config()
 
-// routes
-app.get("/", (req, res) => {
-    res.send("Welcome to my API");
-    function checkUserAuth(req, res, next) {
-  if (req.session.user) return next();
-  return next(new NotAuthorizedError());
-}
-});
+const port = process.env.PORT || 9000
 
-// mongodb connection
+ 
+
+//routes
+app.get('/', (req, res)=>{
+res.send('bem vindo')
+})
+
+// middwares
+app.use('/api' , userRoutes)
+
+
+//mongo db
 mongoose
-    .connect(process.env.mongoClient,
-           {useNewUrlParser: true} )
-    .then(() => console.log("Connected to MongoDB Atlas"))
-    .catch((error) => console.error(error));
+.connect(process.env.MONGODB_URI)
+.then(()=> console.log("conectou"))
+.catch(()=> console.error(error))
 
-// server listening
-app.listen(port, () => console.log("Server listening to", port));
+app.listen(port, ()=> console.log("porta tal", port))
+
+
